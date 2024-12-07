02a16e6866c3d1bd87ae09762748f5a6f93a5da0
strahd <jv.palha@gmail.com>
2024-12-07 11:46:23 -0300

    add: path params requests

--- src/main/java/com/victorpalha/controllers/FirstController.java
+++ src/main/java/com/victorpalha/controllers/FirstController.java
@@ -1,8 +1,9 @@
 package com.victorpalha.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/api")
 @RestController
@@ -11,5 +12,29 @@ public class FirstController {
     @GetMapping("/")
     public String index() {
         return "Hello World";
+    }
+
+    @GetMapping("/users/{id}")
+    public String user(@PathVariable String id) {
+        int idInt = Integer.parseInt(id);
+        return "User with id " + idInt + " found";
+    }
+
+    @GetMapping("/products")
+    public String products(@RequestParam String id) {
+        List<String> products = new ArrayList<String>();
+        products.add("1");
+        products.add("2");
+        products.add("3");
+
+        final boolean productExists = products.contains(id);
+        if (productExists) {
+            for (String product : products) {
+                if (product.equals(id)) {
+                    return product;
+                }
+            }
+        }
+        return "Product not found";
     }
 }
