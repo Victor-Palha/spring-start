ae70a215244b0654163fb4ab96c008a342a55fdf
strahd <jv.palha@gmail.com>
2024-12-07 11:30:12 -0300

    chore: component scan to separate controllers

--- src/main/java/com/victorpalha/controllers/FirstController.java
+++ src/main/java/com/victorpalha/controllers/FirstController.java
@@ -1,4 +1,4 @@
-package com.victorpalha.main;
+package com.victorpalha.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/victorpalha/main/SpringStartApplication.java
+++ src/main/java/com/victorpalha/main/SpringStartApplication.java
@@ -2,8 +2,10 @@ package com.victorpalha.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.victorpalha")
 public class SpringStartApplication {
 
     public static void main(String[] args) {
